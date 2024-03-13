26978c6fcc26ed6c32aae86e51579ca51ab675ca
gauravsh0812 <gauravs@arizona.edu>
2024-03-13 16:09:36 -0700

    updating

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # CleverMath_LLM
 
-```
-python preprocessing/preprocessing.py
-```
+Keep preprocess in the config "True" if running it for the first time. 
+Once you have run it, there is no need to re-process the dataset. 
+The "data/" folder should have the "raw_data" along with the preprocessed and re-arranged dataset i.e.
+"images/", "questions.lst", "labels.lst", and "templates.lst". If you have these files (updated) then no need to 
+re-do these steps. This can avoided by setting the preprocess param to False in the config file.
 
 ```
 python run.py
--- preprocessing/preprocess_dataset.py
+++ preprocessing/preprocess_dataset.py
@@ -14,10 +14,11 @@ from datasets import load_dataset, DownloadConfig, DatasetDict
 with open("config/config.yaml") as f:
     cfg = Box(yaml.safe_load(f))
 
-# opening files in "w" mode
-questions = open("data/questions.lst","w")
-labels = open("data/labels.lst", "w")
-templates = open("data/templates.lst", "w")
+if cfg.general.preprocess:
+    # opening files in "w" mode
+    questions = open("data/questions.lst","w")
+    labels = open("data/labels.lst", "w")
+    templates = open("data/templates.lst", "w")
 
 
 def download_dataset(name):
@@ -38,10 +39,7 @@ def download_dataset(name):
 
     print("downloading the dataset if doesn't exists...")
 
-    raw_data_path = os.path.join(
-        cfg.dataset.data_path,
-        "raw_data"
-    )
+    raw_data_path = "data/raw_data"
 
     dl_config = DownloadConfig(
         resume_download=True, 
@@ -106,12 +104,14 @@ def rearrange_dataset(dataset):
             count+=1
 
             # writing the corresponding questions and labels
+            # the labels ranges from 0-10
+            # the templates are: {addition, adverserail, subtraction, subtraction-multihop}
             questions.write(f"{t_data['question']} \n")
             labels.write(f"{t_data['label']} \n")
             templates.write(f"{t_data['template']} \n")
 
 class Img2MML_dataset(Dataset):
-    def __init__(self, dataframe, vocab, tokenizer):
+    def __init__(self, dataframe, vocab):
         self.dataframe = dataframe
         self.vocab = vocab
 
@@ -119,15 +119,15 @@ class Img2MML_dataset(Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, index):
-        eqn = self.dataframe.iloc[index, 1]
-        indexed_eqn = []
-        for token in eqn.split():
+        qtn = self.dataframe.iloc[index, 1]
+        indexed_qtn = []
+        for token in qtn.split():
             if self.vocab.stoi[token] is not None:
-                indexed_eqn.append(self.vocab.stoi[token])
+                indexed_qtn.append(self.vocab.stoi[token])
             else:
-                indexed_eqn.append(self.vocab.stoi["<unk>"])
+                indexed_qtn.append(self.vocab.stoi["<unk>"])
 
-        return self.dataframe.iloc[index, 0], torch.Tensor(indexed_eqn)
+        return self.dataframe.iloc[index, 0], torch.Tensor(indexed_qtn)
 
 class My_pad_collate(object):
     """
@@ -143,31 +143,29 @@ class My_pad_collate(object):
         self.pad_idx = vocab.stoi["<pad>"]
 
     def __call__(self, batch):
-        _img, _mml = zip(*batch)
-
-        # padding mml
-        # padding to a fix max_len equations with more tokens than
-        # max_len will be chopped down to max_length.
-
-        batch_size = len(_mml)
-        padded_mml_tensors = (
+        _img, _q, _l = zip(*batch)
+        batch_size = len(_q)
+        padded_questions = (
             torch.ones([batch_size, self.max_len], dtype=torch.long)
             * self.pad_idx
         )
         for b in range(batch_size):
-            if len(_mml[b]) <= self.max_len:
-                padded_mml_tensors[b][: len(_mml[b])] = _mml[b]
-            else:
-                padded_mml_tensors[b][: self.max_len] = _mml[b][: self.max_len]
-
+            assert len(_q[b]) <= self.max_len
+            padded_questions[b][: len(_q[b])] = _q[b]
+    
         # images tensors
         _img = torch.Tensor(_img)
+        _l = torch.Tensor(_l, dtype=torch.long)
 
         return (
             _img.to(self.device),
-            padded_mml_tensors.to(self.device),
+            padded_questions.to(self.device),
+
         )
 
+def tokenizer(x):
+    return x.split()
+    
 def create_dataloaders():
 
     print("creating dataloaders...")
@@ -177,44 +175,155 @@ def create_dataloaders():
 
     assert len(q) == len(l) == len(t)
 
+    max_len = max([len(tokenizer(i)) for i in q])
+
     image_num = range(0, len(q))
 
+    # split the image_num into train, test, validate
+    train_val_images, test_images = train_test_split(
+        image_num, test_size=0.1, random_state=42
+    )
+    train_images, val_images = train_test_split(
+        train_val_images, test_size=0.1, random_state=42
+    )
+
+    for t_idx, t_images in enumerate([train_images, test_images, val_images]):
+        qi_data = {
+            "IMG": [num for num in t_images],
+            "QUESTION": [
+                ("<sos> " + q[num] + " <eos>") for num in t_images
+            ],
+            "LABEL": [l[num] for num in t_images],
+        }
     
+        if t_idx == 0:
+            train = pd.DataFrame(qi_data, columns=["IMG", "QUESTION", "LABEL"])
+        elif t_idx == 1:
+            test = pd.DataFrame(qi_data, columns=["IMG", "QUESTION", "LABEL"])
+        else:
+            val = pd.DataFrame(qi_data, columns=["IMG", "QUESTION", "LABEL"])
+    
+    
+    print("saving dataset files to data/ folder...")
+    train.to_csv("data/train.csv", index=False)
+    test.to_csv("data/test.csv", index=False)
+    val.to_csv("data/val.csv", index=False)
+    
+    # build vocab
+    print("building vocab...")
+
+    counter = Counter()
+    for line in train["QUESTION"]:
+        counter.update(line.split())
+
+    # <unk>, <pad> will be prepended in the vocab file
+    vocab = Vocab(
+        counter,
+        min_freq=cfg.dataset.vocab_freq,
+        specials=["<pad>", "<unk>", "<sos>", "<eos>"],
+    )
+
+    # writing vocab file...
+    vfile = open("data/vocab.txt", "w")
+    for vidx, vstr in vocab.stoi.items():
+        vfile.write(f"{vidx} \t {vstr} \n")
+    
+    # initializing pad collate class
+    mypadcollate = My_pad_collate(cfg.general.device, 
+                                  vocab, 
+                                  max_len)
+
+    print("building dataloaders...")
+
+    # initailizing class Img2MML_dataset: train dataloader
+    imml_train = Img2MML_dataset(train, vocab, tokenizer)
+    # creating dataloader
+    if cfg.general.ddp:
+        train_sampler = DistributedSampler(
+            dataset=imml_train,
+            num_replicas=cfg.general.world_size,
+            rank=cfg.general.rank,
+            shuffle=cfg.dataset.shuffle,
+        )
+        sampler = train_sampler
+        shuffle = False
+
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+        
+    train_dataloader = DataLoader(
+        imml_train,
+        batch_size=cfg.training.batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=sampler,
+        collate_fn=mypadcollate,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    # initailizing class Img2MML_dataset: val dataloader
+    imml_val = Img2MML_dataset(val, vocab, tokenizer)
+
+    if cfg.general.ddp:
+        val_sampler = SequentialSampler(imml_val)
+        sampler = val_sampler
+        shuffle = False
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+
+    val_dataloader = DataLoader(
+        imml_val,
+        batch_size=cfg.training.batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=sampler,
+        collate_fn=mypadcollate,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    # initailizing class Img2MML_dataset: test dataloader
+    imml_test = Img2MML_dataset(test, vocab, tokenizer)
+    if cfg.general.ddp:
+        test_sampler = SequentialSampler(imml_test)
+        sampler = test_sampler
+        shuffle = False
+    else:
+        sampler = None
+        shuffle = cfg.dataset.shuffle
+
+    test_dataloader = DataLoader(
+        imml_test,
+        batch_size=cfg.traininig.batch_size,
+        num_workers=cfg.dataset.num_workers,
+        shuffle=shuffle,
+        sampler=None,
+        collate_fn=mypadcollate,
+        pin_memory=cfg.dataset.pin_memory,
+    )
+
+    return train_dataloader, test_dataloader, val_dataloader, vocab
+
 
 if __name__ == "__main__":
-    paths = [
-        "data/raw_data",
-        "data/images"
-    ]
-    for _path in paths:
-        if not os.path.exists(_path):
-            os.mkdir(_path)
-    
-    dataset = download_dataset(name="general")
-    rearrange_dataset(dataset)
-    questions.close()
-    labels.close()
 
-    # dataset_multihop = dataset.filter(
-    #     lambda e:
-    #     e['template'].startswith('subtraction-multihop'), 
-    #     num_proc=4
-    # )
-
-    # dataset_adversarial = dataset.filter(
-    #     lambda e:
-    #     e['template'].startswith('adversarial'), 
-    #     num_proc=4
-    # )
-    # dataset_subtraction = dataset.filter(
-    #     lambda e:
-    #     e['template'].startswith('subtraction'), 
-    #     num_proc=4
-    # )
-    # dataset_addition = dataset.filter(
-    #     lambda e:
-    #     e['template'].startswith('addition'), 
-    #     num_proc=4
-    # )
+    if cfg.general.preprocess:
+        # only if preprocess is True
+        # will re-download, and re-arrange the dataset.
+        # need to be done only first time.
+        paths = [
+            "data",
+            "data/raw_data",
+            "data/images"
+        ]
+        for _path in paths:
+            if not os.path.exists(_path):
+                os.mkdir(_path)
+        
+        dataset = download_dataset(name="general")
+        rearrange_dataset(dataset)
+        questions.close()
+        labels.close()
 
     create_dataloaders()
