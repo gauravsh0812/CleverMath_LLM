ea5e35ee3c7ca3a47bf82128f83ae8f38fdbb70a
gauravsh0812 <gauravs@arizona.edu>
2024-03-28 13:20:38 -0700

    update

--- models/llama2.py
+++ models/llama2.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 import torch
-from transformers import LlamaForSequenceClassification
+from transformers import LlamaModel, LlamaForSequenceClassification
 
 class Llama2Decoder(nn.Module):
     def __init__(self,):
         super(Llama2Decoder, self).__init__()
-        self.model = LlamaForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-hf",
+        self.model = LlamaModel.from_pretrained("meta-llama/Llama-2-7b-hf",
                                                                     token="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")
 
     def forward(self, x):
