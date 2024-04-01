5731bebdd86eba6e504395b115248499dffeb4ed
gauravsh0812 <gauravs@arizona.edu>
2024-03-31 17:19:42 -0700

    updating

--- src/training.py
+++ src/training.py
@@ -54,6 +54,7 @@ def train(
         print("labels: ", labels)
         print("pred_labels: ", pred_labels)
 
+        exit()
         loss = criterion(output.contiguous().view(-1, output.shape[-1]), 
                          labels.contiguous().view(-1))
         
