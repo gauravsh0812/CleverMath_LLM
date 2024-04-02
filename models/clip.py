e74d547c1a78e5744c2be585084630b776397e4f
gauravsh0812 <gauravs@arizona.edu>
2024-04-02 14:36:38 -0700

    updating

--- models/clip.py
+++ models/clip.py
@@ -18,7 +18,6 @@ class ClipVisionEncoder(nn.Module):
     def forward(self, image_paths, device):
 
         _hid, _pool = list(), list()
-        print(image_paths)
         for image_path in image_paths:
             image = Image.open(image_path)
             inputs = self.processor(images=image, return_tensors="pt").to(device)
@@ -28,22 +27,23 @@ class ClipVisionEncoder(nn.Module):
 
             _hid.append(last_hidden_state.squeeze(0))
             _pool.append(pooled_output.squeeze(0))
-            print(last_hidden_state.shape, pooled_output.shape)
+            # print(last_hidden_state.shape, pooled_output.shape)
 
         # hidden: (B, L, 768)
         # pooled: (B, 768)
         return torch.stack(_hid).to(device), torch.stack(_pool).to(device)
 
-configuration={
-      "hidden_size":512,
-      "intermediate_size": 1024,
-      "projection_dim": 512,
-      "num_hidden_layers": 6,
-      "num_attention_heads": 8,
-      "num_channels": 3,
-      "image_size": 224,
-      "patch_size": 32
-      }
 
-cve = ClipVisionEncoder(finetune=True, config=configuration).to("cuda:0")
-cve(["/groups/claytonm/gauravs_data/clevrmath_data/data/images/13704.png"], "cuda:0")
+# configuration={
+#       "hidden_size":512,
+#       "intermediate_size": 1024,
+#       "projection_dim": 512,
+#       "num_hidden_layers": 6,
+#       "num_attention_heads": 8,
+#       "num_channels": 3,
+#       "image_size": 224,
+#       "patch_size": 32
+#       }
+
+# cve = ClipVisionEncoder(finetune=True, config=configuration).to("cuda:0")
+# cve(["/groups/claytonm/gauravs_data/clevrmath_data/data/images/13704.png"], "cuda:0")
