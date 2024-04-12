f02050208739b4c3857197c8e06e094b83734523
gauravsh0812 <gauravs@arizona.edu>
2024-04-12 10:24:26 -0700

    update

--- models/model.py
+++ models/model.py
@@ -63,6 +63,7 @@ class ClevrMath_model(nn.Module):
         masks = self.get_masks(imgs).to(device)  # (B, top_n, w,h)
         # vit_masks = self.vit_encoder(masks) # (B, n_patch, emd_dim)
         vit_imgs = self.vit_encoder(imgs)   # (B, n_patch, emb_dim)
+        exit()
         vit_output = torch.cat((vit_masks, vit_imgs), dim=-1) # (B, n_patches, 2*emb_dim)
         encoutput = self.encadaptor(vit_output)  # (B, max_len, 64)
 
