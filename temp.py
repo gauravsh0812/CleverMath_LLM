722098938b0ecee8b0e465460066e32b4cdbb1c8
gauravsh0812 <gauravs@arizona.edu>
2024-06-14 16:32:27 -0400

    update

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-import torch
+import torch, os
 from torchvision import models
 
 # Load the pre-trained EfficientNet model
@@ -8,18 +8,17 @@ model = models.efficientnet_b0(pretrained=True)
 resnet18 = models.resnet18(pretrained=True)
 encoded_img = nn.Sequential(*(list(resnet18.children())[:-2]))
 
+n = len(os.listdir("/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/"))
+for i in range(n):
+    f = torch.load(f"/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/{i}.pt")[:3,:,:]
+    f = f.unsqueeze(0)
 
-# for i in range():
-i = 9207
-f = torch.load(f"/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/{i}.pt")[:3,:,:]
-f = f.unsqueeze(0)
+    features = model.features(f)
+    print(features.shape)
+    if torch.isnan(features).any():
+        print("features contains NaN:", torch.isnan(features).any())
 
-features = model.features(f)
-print(features.shape)
-if torch.isnan(features).any():
-    print("features contains NaN:", torch.isnan(features).any())
-
-features = encoded_img(f)
-print(features.shape)
-if torch.isnan(features).any():
-    print("features contains NaN:", torch.isnan(features).any())
+    features = encoded_img(f)
+    print(features.shape)
+    if torch.isnan(features).any():
+        print("features contains NaN:", torch.isnan(features).any())
