4d8c189c214170c68d8d8b6e372af683f545bd76
gauravsh0812 <gauravs@arizona.edu>
2024-06-14 16:19:36 -0400

    update

--- temp.py
+++ temp.py
@@ -8,7 +8,7 @@ model = models.efficientnet_b0(pretrained=True)
 resnet18 = models.resnet18(pretrained=True)
 encoded_img = nn.Sequential(*(list(resnet18.children())[:-2]))
 
-f = torch.load("/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/0.pt")[:3,:,:]
+f = torch.load("/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/10.pt")[:3,:,:]
 f = f.unsqueeze(0)
 
 features = model.features(f)
