be5e825d4329c5dca45a5b6e514f0a71a07ffbc5
gauravsh0812 <gauravs@arizona.edu>
2024-06-14 15:52:25 -0400

    update

---
+++ temp.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch
+from torchvision import models
+
+# Load the pre-trained EfficientNet model
+efficientnet = models.efficientnet_b0(pretrained=True)
+
+
+f = torch.load("/groups/claytonm/gauravs_data/clevrmath_data/image_tensors/0.pt")[:3,:,:]
+f = f.unsqueeze(0)
+
+features = model.features(f)
+print(features.shape)
