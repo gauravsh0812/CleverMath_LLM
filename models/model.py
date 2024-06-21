697ad7b5df1efc4cfa8dafd61806544fabb75a2c
gauravsh0812 <gauravs@arizona.edu>
2024-06-21 14:39:15 -0400

    update

--- models/model.py
+++ models/model.py
@@ -133,17 +133,18 @@ class GPT2(nn.Module):
               nn.Linear(features[2], features[3]), nn.BatchNorm1d(max_len), nn.GELU(),
         )
         self.gelu = nn.GELU()
-        self.norm = nn.BatchNorm1d(768)
+        self.norm1 = nn.BatchNorm1d(768)
+        self.norm2 = nn.BatchNorm1d(64)
         self.attn = Self_Attention(64)
 
     def forward(self,xl,xc,xr):
         x = torch.cat((xl,xc), dim=1)  # (B, 100, 768)
-        x = self.gelu(self.norm(self.lin1(x.permute(0,2,1)))).permute(0,2,1)  # (B, max, 768)
+        x = self.gelu(self.norm1(self.lin1(x.permute(0,2,1)))).permute(0,2,1)  # (B, max, 768)
         x = self.lin3(x)    # (B, max, 64)
         x = self.attn(x)  # (B, max, 64)
 
         x = torch.cat((x,xr), dim=1)  # (B, max*2, 64)
-        x = self.gelu(self.norm(self.lin2(x.permute(0,2,1)))).permute(0,2,1)  # (B, max, 64)
+        x = self.gelu(self.norm2(self.lin2(x.permute(0,2,1)))).permute(0,2,1)  # (B, max, 64)
 
         outputs = self.model(input_embeds=x)
         last_hidden_states = outputs.last_hidden_state # (B, L, 768)
