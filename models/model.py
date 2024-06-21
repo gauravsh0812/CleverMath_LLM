c8f23b20ca7033c709a168f53cd96169e4e7a832
gauravsh0812 <gauravs@arizona.edu>
2024-06-21 00:24:42 -0400

    update

--- models/model.py
+++ models/model.py
@@ -1,116 +1,225 @@
-import torch
+import torch, os
 import torch.nn as nn
-import torchvision.models as models
-from transformers import VisualBertConfig, VisualBertModel, GPT2Model
+from PIL import Image
+import yaml
+from box import Box
 
-class ClevrMath_model(nn.Module):
-    def __init__(self,device, num_classes):
-        
-        super(ClevrMath_model, self).__init__()
+from transformers import (
+    CLIPImageProcessor, 
+    CLIPVisionModel, 
+    RobertaModel,
+)
 
-        self.device = device
+with open("config/config_sgpt.yaml") as f:
+    cfg = Box(yaml.safe_load(f))
 
-        # feature extactor
-        resnet18 = models.resnet18(pretrained=True)
-        self.encoded_img = nn.Sequential(*(list(resnet18.children())[:-2]))
-
-        for param in self.encoded_img.parameters():
-            param.requires_grad = False
-
-        # encoded image embedding
-        VB_config = VisualBertConfig.from_pretrained("uclanlp/visualbert-vqa-coco-pre")
-        VB_config.visual_embedding_dim = 512
-        visualbert = VisualBertModel(config=VB_config)
-        self.visual_embedder = visualbert.embeddings.visual_projection
-
-        for param in self.visual_embedder.parameters():
-            param.requires_grad = False
-
-        # qtn embedding
-        gpt = GPT2Model.from_pretrained("openai-community/gpt2")
-        self.word_embeddings = gpt.wte
-        
-        for param in self.word_embeddings.parameters():
-            param.requires_grad = False
-
-        # final_embedding
-        # self.decoder = GPT2Model.from_pretrained("openai-community/gpt2")
-        self.decoder = nn.LSTM(input_size=169,
-                               hidden_size=64,
-                               batch_first=True,
-                               num_layers=1,
-                               dropout=0.1,)
-
-        # for param in self.decoder.parameters():
-        #     param.requires_grad = False
-
-        # pooling
-        self.pool = nn.AdaptiveAvgPool1d(1)
-
-        # layers
-        self.layer = nn.Sequential(
-            nn.Dropout(0.1),
-            nn.Linear(768, 512),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, num_classes),
-        )
+class ClipVisionEncoder(nn.Module):
     
-    def forward(self, imgs, qtn_attns, qtn_ids):
+    def __init__(self,):
+        super(ClipVisionEncoder, self).__init__()
+        self.processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
 
-        # image embedding
-        imgs = imgs.to(self.device)
-        features = self.encoded_img(imgs)
-        if torch.isnan(features).any():
-            print("features contains NaN:", torch.isnan(features).any())
+    def forward(self, image_paths, device):
 
-        features = torch.flatten(features,2,-1)          
-        visual_embeds = self.visual_embedder(features.permute(0,2,1)).to(self.device)
-        visual_token_type_ids = torch.ones(visual_embeds.shape[:-1], dtype=torch.long).to(self.device)
-        visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float).to(self.device)
-        visual_position_id = torch.zeros(visual_embeds.size()[:-1], dtype=torch.long).to(self.device)
-
-        if torch.isnan(visual_embeds).any():
-            print("vis ids contains NaN:", torch.isnan(visual_embeds).any())
+        _hid = list()
+        for image_path in image_paths:
+            image = Image.open(image_path)
+            inputs = self.processor(images=image, return_tensors="pt").to(device)
+            outputs = self.model(**inputs)
+            last_hidden_state = outputs.last_hidden_state
         
-        # qtn embedding
-        qtn_ids = qtn_ids.to(self.device)
-        qtn_attention_mask = qtn_attns.to(self.device)
-        qtn_embeds = self.word_embeddings(qtn_ids)
-        qtn_token_ids = torch.zeros(qtn_embeds.size()[:-1], dtype=torch.long).to(self.device)
-        qtn_position_id = torch.zeros(qtn_embeds.size()[:-1], dtype=torch.long).to(self.device)
+            _hid.append(last_hidden_state.squeeze(0))
+        
+        # hidden: (B, L, 768)
+        return torch.stack(_hid).to(device)#, torch.stack(_pool).to(device)
 
-        # concat: keeping questions first
-        embeds = torch.cat((qtn_embeds, visual_embeds), dim=1).to(self.device)
-        attns = torch.cat((qtn_attention_mask, visual_attention_mask), dim=1).to(self.device)
-        token_ids = torch.cat((qtn_token_ids, visual_token_type_ids), dim=1).to(self.device)
-        pos_ids = torch.cat((qtn_position_id, visual_position_id), dim=1).to(self.device)
-
-        if torch.isnan(embeds).any():
-            print("Embeds contains NaN:", torch.isnan(embeds).any())
-        if torch.isnan(attns).any():
-            print("atns contains NaN:", torch.isnan(attns).any())
-        if torch.isnan(token_ids).any():
-            print("token_ids contains NaN:", torch.isnan(token_ids).any())
-        if torch.isnan(pos_ids).any():
-            print("pos_ids contains NaN:", torch.isnan(pos_ids).any())
-
-        # decoding
-        output,_ = self.decoder(torch.Tensor(embeds).permute(0,2,1))
-        output = torch.Tensor(output).permute(0,2,1)
-        # output = self.decoder(
-        #     inputs_embeds=embeds,
-        #     attention_mask=attns,
-        #     token_type_ids=token_ids,
-        #     position_ids=pos_ids,
-        # )
-        # output = output.last_hidden_state
-        # print("Decoder output:", output.shape)
+def lisa(imgs):
+    tnsrs = []
+    for i in imgs:
+        i = os.path.basename(i).split(".")[0]
+        tnsr = torch.load(f"{cfg.dataset.path_to_data}/lisa/masked_image_tensors/{i}.pt")
+        tnsrs.append(tnsr)
     
-        if torch.isnan(output).any():
-            print("NaNs detected in model output")
-        
-        output = self.pool(output.permute(0,2,1)).permute(0,2,1)
-        output = torch.flatten(output, -2, -1)
-        output = self.layer(output) # (B, num_classes)
+    return torch.stack(tnsrs)
 
-        return output 
+class RobertaEncoder(nn.Module):
+
+    def __init__(self):
+        super(RobertaEncoder, self).__init__()
+        self.model = RobertaModel.from_pretrained("FacebookAI/roberta-base")        
+
+    def forward(self, ids, attns):
+        # shape of ids and attns: (B, max_len)
+        outputs = self.model(input_ids=ids,
+                             attention_mask=attns)
+        last_hidden_states = outputs.last_hidden_state # (B, max_len, 768)
+        return last_hidden_states
+
+class ClipAdaptor(nn.Module):
+    def __init__(self, clip_in_dim, features, max_len):
+        super(ClipAdaptor, self).__init__()
+
+        self.cliplin1 = nn.Linear(clip_in_dim, features[0])
+        self.cliplin2 = nn.Linear(features[0], features[1])
+        self.cliplin3 = nn.Linear(features[1], features[2])
+        self.cliplin4 = nn.Linear(features[2], features[3])
+        self.proj_clip = nn.Linear(50,max_len)
+        self.relu = nn.ReLU()
+    
+    def forward(self, xc):
+        
+        xc = self.relu(self.cliplin1(xc))
+        xc = self.relu(self.cliplin2(xc))
+        xc = self.relu(self.cliplin3(xc))
+        xc = self.relu(self.cliplin4(xc))
+        xc = self.relu(self.proj_clip(xc.permute(0,2,1))).permute(0,2,1)
+        
+        return xc # (B,max_len, 64)
+
+class RobertaAdaptor(nn.Module):
+    def __init__(self, roberta_in_dim, features):
+        super(RobertaAdaptor, self).__init__()
+
+        self.roblin1 = nn.Linear(roberta_in_dim, features[0])
+        self.roblin2 = nn.Linear(features[0], features[1])
+        self.roblin3 = nn.Linear(features[1], features[2])
+        self.roblin4 = nn.Linear(features[2], features[3])
+        self.gelu = nn.GELU()
+    
+    def forward(self, xr):
+        xr = self.gelu(self.roblin1(xr))
+        xr = self.gelu(self.roblin2(xr))
+        xr = self.gelu(self.roblin3(xr))
+        xr = self.gelu(self.roblin4(xr))
+        
+        return xr # (B,max_len, 64)
+
+class LisaAdaptor(nn.Module):
+    def __init__(self, lisa_in_dim, features, max_len):
+        super(ClipAdaptor, self).__init__()
+
+        self.lisalin1 = nn.Linear(lisa_in_dim, features[0])
+        self.lisalin2 = nn.Linear(features[0], features[1])
+        self.lisalin3 = nn.Linear(features[1], features[2])
+        self.lisalin4 = nn.Linear(features[2], features[3])
+        self.proj_lisa = nn.Linear(50,max_len)
+        self.relu = nn.ReLU()
+    
+    def forward(self, xc):
+        
+        xc = self.relu(self.lisalin1(xc))
+        xc = self.relu(self.lisalin2(xc))
+        xc = self.relu(self.lisalin3(xc))
+        xc = self.relu(self.lisalin4(xc))
+        xc = self.relu(self.proj_lisa(xc.permute(0,2,1))).permute(0,2,1)
+        
+        return xc # (B,max_len, 64)
+
+class Projector(nn.Module):
+
+    def __init__(self, features, max_len, num_classes):
+        super(Projector, self).__init__()
+        if self.fusion == "concat":
+            self.final_lin1 = nn.Linear(max_len*2, max_len)
+            self.attn = Self_Attention(features[-1])
+            self.final_lin3 = nn.Linear(features[-1], num_classes)
+            self.norm = nn.BatchNorm1d(features[-1])
+            self.pool = nn.AdaptiveAvgPool1d(1)
+
+        self.gelu = nn.GELU()
+    
+    def combine(self,xc,xr):
+        x = torch.cat((xc,xr), dim=1)  
+        x = self.gelu(self.norm(self.final_lin1(x.permute(0,2,1)))).permute(0,2,1)
+        x = self.attn(x)  # (B, max, 64)
+        return x
+
+    def forward(self, xc, xr, pool=False):
+        x = self.combine(xc,xr)
+        
+        if pool:
+            x = self.pool(x.permute(0,2,1)).permute(0,2,1)  # (B, max_len=1, 64)
+            x = torch.flatten(x, -2,-1)   # (B,max_len*64) >> (B, 64)
+            x = self.gelu(self.final_lin3(x))  # (B, num_classes)
+        
+        return x   # (B, max, 64) or if pool then (B,num_classes)
+
+class Self_Attention(nn.Module):
+    def __init__(self, embed_dim):
+        super(Self_Attention, self).__init__()
+        self.embed_dim = embed_dim
+        self.query = nn.Linear(embed_dim, embed_dim)
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+        self.softmax = nn.Softmax(dim=-1)
+        
+    def forward(self, x):
+        # x shape: (batch_size, seq_length, embed_dim)
+        Q = self.query(x)  # (batch_size, seq_length, embed_dim)
+        K = self.key(x)    # (batch_size, seq_length, embed_dim)
+        V = self.value(x)  # (batch_size, seq_length, embed_dim)
+        
+        # Compute attention scores
+        scores = torch.bmm(Q, K.transpose(1, 2)) / (self.embed_dim ** 0.5)  # (batch_size, seq_length, seq_length)
+        
+        # Apply softmax to get attention weights
+        attention_weights = self.softmax(scores)  # (batch_size, seq_length, seq_length)
+        
+        # Compute the weighted sum of values
+        attention_output = torch.bmm(attention_weights, V)  # (batch_size, seq_length, embed_dim)
+        
+        return attention_output       
+
+class Endoscopic_model(nn.Module):
+
+    def __init__(self, max_len, ans_vocab):
+
+        super(Endoscopic_model, self).__init__()
+        self.clipenc = ClipVisionEncoder()
+        self.robenc = RobertaEncoder()
+        self.lisaadaptor = LisaAdaptor(
+                                480,
+                                cfg.training.adaptor.features,
+                                max_len,
+                            )
+        self.clipadaptor = ClipAdaptor(
+                                768, 
+                                cfg.training.adaptor.features,
+                                max_len,
+                            )
+        self.robertaadaptor = RobertaAdaptor(
+                                cfg.training.roberta.in_dim,
+                                cfg.training.adaptor.features,
+                            )
+        self.projector = Projector(
+                                cfg.training.adaptor.features,
+                                max_len, 
+                                len(ans_vocab),
+                            )
+        for param in self.clipenc.parameters():
+            param.requires_grad = False
+
+        for param in self.robenc.parameters():
+            param.requires_grad = False
+
+    def forward(
+            self, 
+            imgs,
+            qtn_ids,
+            qtn_attns,
+            device,
+        ):
+        
+        lisa_tnsr = lisa(imgs)   # (B, 320, 480)
+        encoded_imgs = self.clipenc(imgs, device)  # (B, L=w*h, dim)
+        last_hidden_roberta = self.robenc(qtn_ids, qtn_attns) # (B, max_len, 768)   
+
+        lisaoutput = self.lisaadaptor(lisa_tnsr)    # (B, max_len, 64) 
+        clipoutput = self.clipadaptor(encoded_imgs)  # (B, max_len, 64)
+        visionoutput = self.projector(lisaoutput, clipoutput)
+
+        roboutput = self.robertaadaptor(last_hidden_roberta) # (B, max, 64)
+        projoutput = self.projector(visionoutput, roboutput, pool=True) # (B,num_classes)
+        
+        return projoutput
